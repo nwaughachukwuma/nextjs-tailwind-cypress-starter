bae1be04be909638b8869dc6f04d995f87bc0eb4
Chukwuma Nwaugha <nwaughac@gmail.com>
2021-08-28 10:52:14 +0400

    Add husky and lint-staged

--- cypress/integration/example.spec.ts
+++ cypress/integration/example.spec.ts
@@ -1,12 +1,12 @@
 // cypress/integration/app.spec.ts
 
 describe('Example Spec', () => {
-    it('should find page title', () => {
-      // Start from the index page
-      cy.visit('http://localhost:3000/')
+  it('should find page title', () => {
+    // Start from the index page
+    cy.visit('http://localhost:3000/')
 
-      cy.get('title').contains('Create Next App')
-    })
+    cy.get('title').contains('Create Next App')
   })
+})
 
 export {}
