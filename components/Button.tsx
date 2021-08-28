588ebe3fa95d2e49f0169a21ab530a7ca6f6d24a
Chukwuma Nwaugha <nwaughac@gmail.com>
2021-08-28 18:41:51 +0400

    Clean-up

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,5 +1,9 @@
-import styles from '../styles/Components.module.scss'
-
 export default function Button({ title }) {
-  return <button className={styles.button}>{title}</button>
+  return (
+    <div className="w-full flex flex-col items-center space-y-5 py-10">
+      <button className="block p-3 bg-gray-800 bg-opacity-80 text-white font-mono w-[80%] rounded-md cursor-pointer uppercase border-none outline-none hover:bg-black transition-all duration-[400ms] transform hover:scale-105 hover:bg-opacity-100 active:translate-y-[4px]">
+        {title}
+      </button>
+    </div>
+  )
 }
