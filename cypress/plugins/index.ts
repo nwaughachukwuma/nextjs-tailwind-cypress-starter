dfd2ae479bbbb46b651ed9362326b1603040e740
Chukwuma Nwaugha <nwaughac@gmail.com>
2021-08-28 04:44:17 +0400

    Rename .js files to .ts

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -16,7 +16,9 @@
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
-module.exports = (on, config) => {
+const config: Cypress.PluginConfig = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
+
+export default config
